b088ddf9ca8ce4f5207afabdb971bcab41349145
Sajana Weerakoon <weerakoonsajana@gmail.com>
2021-05-23 17:35:39 +0530

    Finalazing the Algorithm

--- Missionaries_Cannibals.js
+++ Missionaries_Cannibals.js
@@ -10,6 +10,31 @@ var solutions = []
 //possible actions inside the boat when going 
 const operations = [[1,0,1],[0,1,1],[2,0,1],[0,2,1],[1,1,1]]
 
+//Utility Functions
+function arrayEquals(a, b) {
+    return Array.isArray(a) &&
+      Array.isArray(b) &&
+      a.length === b.length &&
+      a.every((val, index) => val === b[index]);
+  }
+
+function arrayAlreadyHasArray(arr, testArr){
+    for(var i = 0; i<arr.length; i++){
+        let checker = []
+        for(var j = 0; j<arr[i].length; j++){
+            if(arr[i][j] === testArr[j]){
+                checker.push(true)
+            } else {
+                checker.push(false)
+            }
+        }
+        if (checker.every(check => check === true)){
+            return true
+        }
+    }
+    return false
+}
+
 //Function to execute operations
 const do_operation = (state, operation) =>{
     try {
@@ -71,3 +96,46 @@ const isStateSafe = (state) => {
     else
     return false;
 }
+
+
+//Determining the next possible states available
+const nextPossibleStates = (state,operationsList) =>{
+    var nextPossibleRemainingStates=[]
+    var nextState;
+    operationsList.forEach(op => {
+        nextState =do_operation(state,op)
+       if(isLegal(nextState) && isStateSafe(nextState)) {
+            nextPossibleRemainingStates.push(nextState)
+       }
+    })
+    return nextPossibleRemainingStates
+}
+
+//Fucntion to solve the problem 
+const run =(nextState, path) => {
+    var copyOfPath = [...path]
+    if(arrayEquals(nextState,endState)){
+        copyOfPath.push(nextState)
+        solutions.push(copyOfPath)
+        return true
+    }
+
+    else if (arrayAlreadyHasArray(path,nextState)){
+        return 
+    }
+        
+    else{
+        copyOfPath.push(nextState)
+        nextPossibleStates(nextState,operations).forEach(st => {
+            run(st,copyOfPath)
+        })
+    } 
+}
+
+//Main function 
+const Main = () =>{
+    run(startState,[])
+    console.log(solutions)
+}
+
+Main();
